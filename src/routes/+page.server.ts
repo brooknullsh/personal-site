de4590be50a7b4a7c59852fc1249d41476f5d695
Brook Nash <brooknullsh@gmail.com>
2025-02-08 12:42:06 +0000

    chore: formatting & linting

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,20 @@
 import type { PageServerLoad } from "./$types";
 
+type BlogMetadata = {
+  slug: string;
+  layout: string;
+  title: string;
+  description: string;
+  published: string;
+};
+
 export const load: PageServerLoad = async () => {
   const blogFiles = import.meta.glob<{ metadata: Record<string, string> }>("./blog/*/*.svx");
 
-  const parsedObjects = Object.entries(blogFiles).map(async ([fileName, component]) => {
-    return { slug: fileName.split("/").at(2), ...(await component()).metadata };
-  });
+  const parsedObjects = Object.entries(blogFiles).map(async ([fileName, component]) => ({
+    slug: fileName.split("/").at(2),
+    ...(await component()).metadata,
+  }));
 
-  return { allMetadata: await Promise.all(parsedObjects) };
+  return { allMetadata: (await Promise.all(parsedObjects)) as BlogMetadata[] };
 };

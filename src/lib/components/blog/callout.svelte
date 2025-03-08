4bfa80f3efbbbb223c0b85d51dd021afa5d586c5
Brook Nash <brooknullsh@gmail.com>
2025-03-08 16:09:45 +0000

    refactor: simplify dynamic classes

--- src/lib/components/blog/callout.svelte
+++ src/lib/components/blog/callout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import type { Snippet } from "svelte";
-  import { cn } from "$lib/utils";
 
   type Props = {
     children: Snippet;
@@ -8,24 +7,13 @@
   };
 
   let { children, type }: Props = $props();
+  const colour = type === "info" ? "blue" : type === "warn" ? "yellow" : "red";
+
+  const typeContainerStyle = () => `border-${colour}-500 bg-${colour}-500/10`;
+  const typeTextStyle = () => `text-${colour}-500`;
 </script>
 
-<div
-  class={cn(
-    "flex w-full gap-2 rounded border-l-4 p-4",
-    type === "info" && "border-blue-500 bg-blue-500/10",
-    type === "warn" && "border-yellow-500 bg-yellow-500/10",
-    type === "alert" && "border-red-500 bg-red-500/10",
-  )}
->
-  <p
-    class={cn(
-      type === "info" && "text-blue-500",
-      type === "warn" && "text-yellow-500",
-      type === "alert" && "text-red-500",
-    )}
-  >
-    {type === "info" ? "🙋‍♂️" : type === "warn" ? "⚠️" : "🚨"}
-    {@render children()}
-  </p>
+<div class={`flex w-full gap-2 rounded border-l-4 p-4 ${typeContainerStyle()}`}>
+  <span class="flex items-center">{type === "info" ? "🙋‍♂️" : type === "warn" ? "⚠️" : "🚨"}</span>
+  <p class={typeTextStyle()}>{@render children()}</p>
 </div>

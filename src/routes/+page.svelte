62023286464591851cfc9e3244e457686ca84d50
Brook Nash <brooknullsh@gmail.com>
2025-04-23 15:50:36 +0100

    feat: quote from first blog

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,3 +8,12 @@
   My name is Brook Nash, a Software Engineer currently working for a consultancy based in Wales,
   United Kingdom working on extremely legacy code and architecture.
 </p>
+
+<p class="font-extralight italic">
+  "My main reason for this site in general, even ignoring the blog aspect of it, is to store my
+  findings, resources & awful code snippets. 'Why not use a note-taking app?' you may ask, well in
+  the classic developer sense I am not happy with any of them and I do not have enough control over
+  them."
+  <br />
+  <a class="text-blue-400 hover:underline" href="/blog/hello-world">Hello, World! &#8599</a>
+</p>

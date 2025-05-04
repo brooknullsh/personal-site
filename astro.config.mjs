d697e769824a9cfd3067abd632076477d962cbc8
Brook Nash <brooknullsh@gmail.com>
2025-05-04 22:24:25 +0100

    feat: dynamic syntax highlighting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
-export default defineConfig({ vite: { plugins: [tailwindcss()] } });
+export default defineConfig({
+  vite: { plugins: [tailwindcss()] },
+  markdown: {
+    shikiConfig: { themes: { light: "one-light", dark: "vitesse-dark" } },
+  },
+});

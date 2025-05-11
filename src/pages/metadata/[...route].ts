da2f2f5cc97bddf6c59bb2de4cff86a8051f007a
Brook Nash <brooknullsh@gmail.com>
2025-05-11 19:55:56 +0100

    fix: decrease logo size for metadata

--- src/pages/metadata/[...route].ts
+++ src/pages/metadata/[...route].ts
@@ -12,7 +12,7 @@ export const { getStaticPaths, GET } = OGImageRoute({
     description: page.subtitle,
     bgGradient: [[255, 255, 255]],
     fonts: ["./src/assets/geist-mono.ttf"],
-    logo: { path: "./public/favicon.png" },
+    logo: { size: [36], path: "./public/favicon.png" },
     font: {
       title: { size: 120, weight: "Bold", color: [0, 0, 0] },
       description: { color: [0, 0, 0] },

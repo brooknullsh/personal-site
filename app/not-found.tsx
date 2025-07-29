6136d826475bcf3b79e95503fad4bd1f8909ffb5
Brook Nash <brooknullsh@gmail.com>
2025-07-29 18:39:53 +0100

    revert: formatting

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,28 +5,26 @@ import Link from "next/link"
 
 export default function NotFound()
 {
-    return (
-        <main className="flex flex-grow flex-col gap-12">
-            <Box>
-                <header className="flex items-center justify-between p-2">
-                    <Link className="text-secondary" id="home-btn" href="/">
-                        &lsaquo; Home
-                        <Loader>
-                            <Shortcut target="q" id="home-btn" />
-                        </Loader>
-                    </Link>
-                    <p className="text-muted">Brook Nash</p>
-                </header>
-            </Box>
+  return (
+    <main className="flex flex-grow flex-col gap-12">
+      <Box>
+        <header className="flex items-center justify-between p-2">
+          <Link className="text-secondary" id="home-btn" href="/">
+            &lsaquo; Home
+            <Loader>
+              <Shortcut target="q" id="home-btn" />
+            </Loader>
+          </Link>
+          <p className="text-muted">Brook Nash</p>
+        </header>
+      </Box>
 
-            <div className="text-center">
-                <h2 className="text-2xl font-bold tracking-tighter">
-                    Nothing here
-                </h2>
-                <p className="text-muted">
-                    The page you are looking for does not exist.
-                </p>
-            </div>
-        </main>
-    )
+      <div className="text-center">
+        <h2 className="text-2xl font-bold tracking-tighter">Nothing here</h2>
+        <p className="text-muted">
+          The page you are looking for does not exist.
+        </p>
+      </div>
+    </main>
+  )
 }
--- lib/index.ts
+++ lib/index.ts
@@ -4,32 +4,32 @@ import path from "path"
 
 export async function getNotes()
 {
-    const dirPath = path.join(process.cwd(), "notes")
-    const fileNames = fs.readdirSync(dirPath)
+  const dirPath = path.join(process.cwd(), "notes")
+  const fileNames = fs.readdirSync(dirPath)
 
-    const notes = fileNames.map(async (name) =>
-    {
-        const content = fs.readFileSync(path.join(dirPath, name), "utf-8")
-        const regex = /---\s*([\s\S]*?)\s*---/
+  const notes = fileNames.map(async (name) =>
+  {
+    const content = fs.readFileSync(path.join(dirPath, name), "utf-8")
+    const regex = /---\s*([\s\S]*?)\s*---/
 
-        const source = await serialize(content, { parseFrontmatter: true })
-        const metadata = source.frontmatter as {
-            title: string
-            subtitle: string
-            published: string
-        }
+    const source = await serialize(content, { parseFrontmatter: true })
+    const metadata = source.frontmatter as {
+      title: string
+      subtitle: string
+      published: string
+    }
 
-        return {
-            content,
-            markdown: content.replace(regex, "").trim(),
-            metadata,
-            slug: name.slice(0, name.length - 3),
-        }
-    })
+    return {
+      content,
+      markdown: content.replace(regex, "").trim(),
+      metadata,
+      slug: name.slice(0, name.length - 3),
+    }
+  })
 
-    return (await Promise.all(notes)).sort(
-        (a, b) =>
-            new Date(b.metadata.published).getTime() -
-            new Date(a.metadata.published).getTime(),
-    )
+  return (await Promise.all(notes)).sort(
+    (a, b) =>
+      new Date(b.metadata.published).getTime() -
+      new Date(a.metadata.published).getTime(),
+  )
 }
